4f77576710eb82a8740ea47ff1393ec2e98f85a7
Oleh <olvolo17@gmail.com>
2024-07-07 13:58:56 +0300

    Create the common Gradle files and fix gradle.kts

---
+++ config-service/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
+    }
+}
+
+bootJar {
+    mainClassName = "org.example.config.ConfigApplication"
+}
